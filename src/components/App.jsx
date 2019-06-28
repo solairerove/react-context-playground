64340fd8353944edc161689589ef6fae8de3dc96
Mikita Kryvitski <solairerove@gmail.com>
2019-06-28 19:18:15 +0300

    feature: init brand new app

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <h1>Title</h1>
+        <p>Content</p>
+      </div>
+    );
+  }
+}
+
+export default App;
